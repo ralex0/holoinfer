72478faf86848018c9be8c369a3b681e770c72bd
ralex0 <ralex0@users.noreply.github.com>
2017-05-23 14:52:19 -0400

    Add a setup.py to offically start tracking version

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='holoinfer',
+      version='0.1.0',
+      description='Hologram analysis by way of Bayesian inference',
+      url='http://github.com/ralex0/hello-world',
+      author='ralex0',
+      author_email='ralex0@users.noreply.github.com',
+      license='MIT',
+      packages=['holoinfer'])
