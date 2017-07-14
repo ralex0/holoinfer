fe38d0e10932f65c60609045827207a4337e9f47
ralex0 <ralex0@users.noreply.github.com>
2017-07-13 21:25:50 -0400

    Update Rayleigh-Gans Sphere model
    Fixed a minus sign, added 1, and updated documentation

--- holoinfer/model/hologram.py
+++ holoinfer/model/hologram.py
@@ -9,17 +9,21 @@ def rayleigh_gans_sphere_holo(position, k, center, m, a):
     z = center[2]
     theta = np.arccos(z / r)
 
+    V = 4 / 3 * np.pi * a ** 3
+
     x = k * a
     u = 2 * x * np.sin(.5 * theta)
-    g = 3 / u ** 3 * (np.sin(u) - u * np.cos(u))
+    G = 3 / u ** 3 * (np.sin(u) - u * np.cos(u))
 
     re_m, im_m = m[0], m[1]
 
-    s2_mag = k ** 3 * a ** 3 * np.sqrt(re_m * re_m + im_m * im_m - 2 * re_m + 1) * 2 / 3 * np.abs(np.cos(theta)) * g
-    s2_phase = np.arctan((re_m - 1) / im_m)
+    S2_mag = k ** 3 * a ** 3 * np.sqrt(re_m * re_m + im_m * im_m - 2 * re_m + 1) * 2 / 3 * np.cos(theta) * G
+    S2_phase = np.arctan((1 - re_m) / im_m) if im_m != 0 else np.pi / 2
 
-    holo_scatt = (1 / (k * r) * s2_mag) ** 2
-    holo_inter = 2 * 1 / (k * r) * s2_mag * np.abs(np.sin(k * (r - z) + s2_phase))
+    holo_scatt = (1 / (k * r) * S2_mag) ** 2
+    holo_inter = 2 * 1 / (k * r) * np.sqrt(S2_mag ** 2) * np.sin(k * (r - z) + S2_phase)
 
-    return holo_scatt + holo_inter
+    holo_full = holo_scatt + holo_inter + 1
+
+    return holo_full
 
